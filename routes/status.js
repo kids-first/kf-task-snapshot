85f0d4e6c58fbdea1e0811d56969f5e55c3040f1
liberaliscomputing <liberalis.computing@gmail.com>
2018-07-12 10:36:01 -0400

    :construction: refactoring

--- routes/status.js
+++ routes/status.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
     return res.status(200).json({ 
         message: {
-            name: 'Snapshot Task',
+            name: 'snapshot task',
             version: '1.0.0'
         }
     });

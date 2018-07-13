c179fb20770d0d39a979d3c97bbd8a31d92d2bb6
liberaliscomputing <liberalis.computing@gmail.com>
2018-07-13 12:17:48 -0400

    :recycle: refactor unit testing for GET /status endpoint with request

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,23 +1,27 @@
 'use strict';
 
 const expect = require('chai').expect;
-const request = require('request-promise');
+const request = require('request');
 
 
 const host = process.env.HOST || 'localhost';
-const port = process.env.PORT || 3000;
-const options = { resolveWithFullResponse: true }; 
+const port = process.env.PORT || 3030;
+const options = { 
+    baseUrl: `http://${host}:${port}`,
+    headers: {
+        'content-type': 'application/json'
+    }
+};       
 
 describe('Sending a request', () => {
     describe('GET /status', () => {
+        options.url = '/status'
         options.method = 'GET';
-        options.uri = `http://${host}:${port}/status`
         
         it('should return statusCode equal to 200', () => {
-            request(options)
-                .then((res) => {
-                    expect(res.statusCode).to.equal(200);
-                });
+            request(options, (err, res, data) => {
+                expect(res.statusCode).to.equal(200);
+            })
         });
     });
 });

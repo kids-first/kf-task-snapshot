a7069b8ed1bb6b7eb1dc768ecd5104c07afccf77
liberaliscomputing <liberalis.computing@gmail.com>
2018-07-23 12:12:21 -0400

    :tada: rename status to index and add GET / endpoint

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const logErrors = require('./middlewares/errorHandler');
-const statusRouter = require('./routes/status');
-const tasksRouter = require('./routes/tasks');
+const indexRouter = require('./routes/index');
+const tasksRouter = require('./routes/tasks')
 
 
 const app = express();
@@ -18,7 +18,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(logErrors);
-app.use('/status', statusRouter);
+app.use('/', indexRouter);
+app.use('/status', indexRouter);
 app.use('/tasks', tasksRouter);
 
 module.exports = app;
